29dd75c3433eb90cf6170f8bfba4a9623af3efd6
Ramon Ribeiro <rhpr@cesar.org.br>
2018-10-25 14:27:01 -0300

    Create Sign in frontend
    Change-Id: Ifd40ad19c620c00ebcb88aa158efda4a5a74dc36

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
+import '../styles/Signin.css';
 
 class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignin = this.handleSignin.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.id]: e.target.value });
+  }
+
+  handleSignin(e) {
+    const { email, password } = this.state;
+    e.preventDefault();
+    // TODO: Request to authenticator
+    alert(`${email}\n${password}`);
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
-      <div>
-        <h1>Sign-in page</h1>
+      <div className="Signin">
+        <form onSubmit={e => this.handleSignin(e)}>
+          <input className="Text-input" id="email" type="email" value={email} onChange={this.handleChange} placeholder="Email" required />
+          <br />
+          <input className="Text-input" id="password" type="password" value={password} onChange={this.handleChange} placeholder="Password" required />
+          <br />
+          <input className="Button" id="button-signin" type="submit" value="Sign in" />
+          <br />
+        </form>
+        <a href="/signup">
+          <input className="Button ButtonSmall" id="button-signup" type="submit" value="Sign up" />
+        </a>
+        <a href="/forgot">
+          <input className="Button ButtonSmall" id="button-forgot" href="/forgot" type="submit" value="Forgot password?" />
+        </a>
       </div>
     );
   }
