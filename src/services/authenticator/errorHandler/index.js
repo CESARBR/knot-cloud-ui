fd15ce162a173f2e13b08d330e76522fed2d407e
Ramon Ribeiro <rhpr@cesar.org.br>
2018-11-05 14:28:31 -0300

    Add ErrorHandler service
    Change-Id: Icff471fd67a14c7f3ab06306fe82a4396ce9ca55

---
+++ src/services/authenticator/errorHandler/index.js
@@ -0,0 +1,30 @@
+class ErrorHandler {
+  static async execute(promise) {
+    try {
+      await promise;
+    } catch (error) {
+      if (error.response) {
+        // Request was made and server responded with a status code
+        // out of range 2XX
+        const { status, data: { message: dataMessage } } = error.response;
+
+        switch (status) {
+          case 400:
+            throw Error('Bad request');
+          case 500:
+            throw Error('System error');
+          default:
+            throw Error(dataMessage);
+        }
+      } else if (error.request) {
+        // Request was made but no response was received
+        throw Error('Could not reach server! Try again later');
+      } else {
+        // Something happened when setting up the request
+        throw error.message;
+      }
+    }
+  }
+}
+
+export default ErrorHandler;
