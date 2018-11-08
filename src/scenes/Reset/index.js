69ad62b49143c78ebef292880464c88ef731001d
Paulo Serra Filho <ptvsf@cesar.org.br>
2018-11-08 10:21:06 -0300

    Change password check behavior
    Checking if the password and confirmation match right after input.
    Added rules to avoid no match messages if fields are empty.
    
    Change-Id: Ifb95b427e83961a9464a63f38d408b11e003d6b3

--- src/scenes/Reset/index.js
+++ src/scenes/Reset/index.js
@@ -2,29 +2,49 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import TextInput from 'components/TextInput';
 import PrimaryButton from 'components/Button/PrimaryButton';
+import ErrorMessage from 'components/ErrorMessage';
 import './styles.css';
 
 class Reset extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      redirect: false
+      redirect: false,
+      passwordMatch: false,
+      errorMessage: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkPasswordMatch = this.checkPasswordMatch.bind(this);
   }
 
   handleSubmit(e) {
-    const { password } = this.state;
-    const { confirm } = this.state;
+    const { passwordMatch } = this.state;
 
-    if (password !== confirm) {
-      window.alert("Passwords don't match!"); // eslint-disable-line no-alert
-    } else {
-      window.alert('Password updated!'); // eslint-disable-line no-alert
+    if (passwordMatch) {
       this.setState({
         redirect: true
       });
     }
+
+    e.preventDefault();
+  }
+
+  checkPasswordMatch(e) {
+    const { password } = this.state;
+    const confirmPassword = e.target.value;
+    let passwordMatch = false;
+    let emptyFields = true;
+
+    passwordMatch = password === confirmPassword;
+    emptyFields = !(password && confirmPassword);
+
+    const errorMessage = (passwordMatch || emptyFields) ? '' : "Passwords don't match!";
+
+    this.setState({
+      passwordMatch,
+      errorMessage
+    });
+
     e.preventDefault();
   }
 
@@ -37,12 +57,15 @@ class Reset extends Component {
   }
 
   render() {
+    const { errorMessage } = this.state;
+
     return (
       <div className="reset-pwd-wrapper">
         <h3 className="page-title"> RESET PASSWORD </h3>
         <form className="reset-form" onSubmit={e => this.handleSubmit(e)}>
           <TextInput type="password" id="new-password" placeholder="New Password" onChange={e => this.setState({ password: e.target.value })} />
-          <TextInput type="password" id="new-password-confirm" placeholder="Confirm new password" onChange={e => this.setState({ confirm: e.target.value })} />
+          <TextInput type="password" id="new-password-confirm" placeholder="Confirm new password" onChange={e => this.checkPasswordMatch(e)} />
+          <ErrorMessage message={errorMessage} />
           <PrimaryButton name="SUBMIT" />
         </form>
         {this.renderRedirect()}
