977ff58052994573c1a1a6082269535f9a8b8798
Paulo Serra Filho <ptvsf@cesar.org.br>
2018-11-08 10:21:06 -0300

    Change password check behavior
    Checking if the password and confirmation match right after input.
    Added rules to avoid no match messages if fields are empty.
    
    Change-Id: Ifb95b427e83961a9464a63f38d408b11e003d6b3

--- src/scenes/Sign/Register/index.js
+++ src/scenes/Sign/Register/index.js
@@ -5,6 +5,7 @@ import Authenticator from 'services/Authenticator';
 import ErrorMessage from 'components/ErrorMessage';
 import { PrimaryButton, SecondaryButton } from 'components/Button';
 import TextInput from 'components/TextInput';
+import CheckPassword from 'services/CheckPassword';
 import 'scenes/Sign/styles.css';
 
 class Signup extends Component {
@@ -12,8 +13,7 @@ class Signup extends Component {
     super(props);
     this.state = {
       email: '',
-      password: '',
-      confirmPassword: '',
+      passwordMatch: false,
       redirect: false,
       errorMessage: ''
     };
@@ -27,10 +27,11 @@ class Signup extends Component {
   }
 
   handleSignup(e) {
-    const { email, password, confirmPassword } = this.state;
+    const { email, password, passwordMatch } = this.state;
     const authService = new Authenticator(config.get('authenticator.host'), config.get('authenticator.port'));
+
     e.preventDefault();
-    if (password === confirmPassword) {
+    if (passwordMatch) {
       authService.createUser(email, password)
         .then(() => {
           this.setState({ redirect: true });
@@ -38,9 +39,44 @@ class Signup extends Component {
         .catch((error) => {
           this.setState({ errorMessage: error.message });
         });
-    } else {
-      this.setState({ errorMessage: 'Password not match' });
     }
+  }
+
+  checkPasswordOK(password, confirmPassword) {
+    const checkPasswordService = new CheckPassword();
+    const { passwordMatch, emptyFields }  = checkPasswordService.isPasswordOK(password, confirmPassword);
+    // The error message should NOT appear if the passwords match or if there
+    // are empty fields
+    const errorMessage = (passwordMatch || emptyFields) ? '' : "Passwords don't match!";
+
+    this.setState({
+      passwordMatch,
+      errorMessage
+    });
+  }
+
+  checkPassword(e) {
+    const { confirmPassword } = this.state;
+    const password = e.target.value;
+
+    this.checkPasswordOK(password, confirmPassword);
+    this.setState({
+      password
+    })
+
+    e.preventDefault();
+  }
+
+  checkConfirmPassword(e) {
+    const { password } = this.state;
+    const confirmPassword = e.target.value;
+
+    this.checkPasswordOK(password, confirmPassword);
+    this.setState({
+      confirmPassword
+    })
+
+    e.preventDefault();
   }
 
   renderRedirect() { // eslint-disable-line consistent-return
@@ -56,8 +92,8 @@ class Signup extends Component {
       <div className="sign-form">
         <form onSubmit={e => this.handleSignup(e)}>
           <TextInput type="email" id="email" placeholder="Email" onChange={this.handleChange} />
-          <TextInput type="password" id="password" placeholder="Password" onChange={this.handleChange} />
-          <TextInput type="password" id="confirmPassword" placeholder="Confirm Password" onChange={this.handleChange} />
+          <TextInput type="password" id="password" placeholder="Password" onChange={e => this.checkPassword(e)} />
+          <TextInput type="password" id="confirmPassword" placeholder="Confirm Password" onChange={e => this.checkConfirmPassword(e)} />
           <ErrorMessage message={errorMessage} />
           <PrimaryButton name="Sign up" />
         </form>
