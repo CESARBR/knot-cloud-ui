c80522767bd8d4adf48d4c7ea498afc6fa3a72b7
João Neto <joao.alexandre@cesar.org.br>
2018-11-13 12:35:38 -0300

    Add session sign-out
    Change-Id: Id9ede3d1d0799ccf61b2f0c61df582236bf880d8

--- src/scenes/Gateways/index.js
+++ src/scenes/Gateways/index.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, Redirect } from 'react-router-dom';
 import KHeadIcon from '_assets/png/KHeadIcon.png';
 import AddIcon from '_assets/svg/AddIcon.svg';
 import Cardboard from 'scenes/Gateways/components/CardBoard';
 import Card from 'scenes/Gateways/components/Card';
+import Storage from 'services/Storage';
 import './styles.css';
 
 class Gateway extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false
+    };
+
+    this.handleSignOut = this.handleSignOut.bind(this);
+  }
+
+  handleSignOut(event) {
+    const storage = new Storage();
+
+    storage.removeToken();
+    this.setState({ redirect: true });
+
+    event.preventDefault();
+  }
+
+  renderRedirect() { // eslint-disable-line consistent-return
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/login" />;
+    }
+  }
+
   render() {
     return (
       <div>
@@ -18,7 +44,7 @@ class Gateway extends Component {
               <li><NavLink to="/gateway">Gateways</NavLink></li>
               <li><NavLink to="/dev">Devices</NavLink></li>
               <li><NavLink to="/admin">Administration</NavLink></li>
-              <li><Link to="/">Sign out</Link></li>
+              <li><Link to="" onClick={this.handleSignOut}>Sign out</Link></li>
             </div>
           </ul>
         </div>
@@ -32,6 +58,7 @@ class Gateway extends Component {
           <Card name="Gateway2" uuid="333" token="333" />
           <Card name="App2" uuid="444" token="444" />
         </Cardboard>
+        {this.renderRedirect()}
       </div>
     );
   }
