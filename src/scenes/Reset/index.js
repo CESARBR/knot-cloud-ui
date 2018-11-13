f23a3e09cfd7b89cd0dd1ddb71540ded6bb5c1b9
Paulo Serra Filho <ptvsf@cesar.org.br>
2018-11-13 13:53:34 -0300

    Create Home component
    Changed default "/" path to render Home component. Signin is now on
    "/signin" path.
    All componentes were properly changed to point to the correct path
    as needed.
    
    Change-Id: I04f78596a58eb4a560154539465cef377b93568e

--- src/scenes/Reset/index.js
+++ src/scenes/Reset/index.js
@@ -89,7 +89,7 @@ class Reset extends Component {
     const { redirect } = this.state;
 
     if (redirect) {
-      return <Redirect to="/" />;
+      return <Redirect to="/signin" />;
     }
   }
 
