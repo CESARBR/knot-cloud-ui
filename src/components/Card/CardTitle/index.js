3c32f16cfd95d08d393c462d8fb2be3f190db483
Ramon Ribeiro <rhpr@cesar.org.br>
2018-12-12 14:43:48 -0300

    Create Card component
    The Card has the following sub-components:
      - CardHeader
        - CardTitle
      - CardBody
    The structure to create a Card component is the following:
    <Card ...>
      <CardHeader ...>
        <CardTitle ...>
        </CardTitle>
      </CardHeader>
      <CardBody ...>
      </CardBody>
    </Card>
    
    Change-Id: Id92abad36f3992c20e668e0c9f00b9b091cce8c9

---
+++ src/components/Card/CardTitle/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './styles.css';
+
+class CardTitle extends Component {
+  constructor(props) {
+    super(props);
+    const { title } = this.props;
+    this.state = { title };
+  }
+
+  handleBlur(e) {
+    const newTitle = e.target.textContent;
+    const { onTitleChange } = this.props;
+    const { title } = this.state;
+
+    if (newTitle !== title) {
+      this.setState({ title: newTitle });
+      onTitleChange(newTitle);
+    }
+  }
+
+  handleKeyPress(e) {
+    if (e.key === 'Enter') { e.target.blur(); }
+  }
+
+  render() {
+    const { editable, marginLeft } = this.props;
+    const { title } = this.state;
+    return (
+      <div
+        role="textbox"
+        tabIndex="0"
+        onKeyPress={e => this.handleKeyPress(e)}
+      >
+        <h1
+          contentEditable={editable}
+          suppressContentEditableWarning
+          className="card-title"
+          onBlur={e => this.handleBlur(e)}
+          style={{ marginLeft }}
+        >
+          {title}
+        </h1>
+      </div>
+    );
+  }
+}
+
+CardTitle.defaultProps = {
+  editable: false,
+  onTitleChange: null
+};
+
+CardTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+  marginLeft: PropTypes.string.isRequired,
+  editable: PropTypes.bool,
+  onTitleChange: PropTypes.func
+};
+
+export default CardTitle;
