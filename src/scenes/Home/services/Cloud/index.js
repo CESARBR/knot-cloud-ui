da213f8ba78c78385240a471596d7391b96dfd39
Paulo Serra Filho <ptvsf@cesar.org.br>
2018-12-19 16:03:01 -0300

    Create service to connect with KNoTCloudWebSocket
    This service is responsible for handling the operations with the KNoT
    Cloud through the knot-cloud-websocket library.
    
    Change-Id: I08755b5605c5f9886d9bd94d512bcf0d00255ac6

---
+++ src/scenes/Home/services/Cloud/index.js
@@ -0,0 +1,14 @@
+import * as KNoTCloudWebSocket from '@cesarbr/knot-cloud-websocket';
+
+class Cloud {
+  constructor(hostname, port, uuid, token) {
+    this.client = new KNoTCloudWebSocket({
+      hostname,
+      port,
+      uuid,
+      token
+    });
+  }
+}
+
+export default Cloud;
